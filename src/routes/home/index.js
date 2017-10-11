91901551a0787bb9022f1116a00c86c476773f22
Jose Santos <josemsantos1994@gmail.com>
2017-10-11 01:13:03 -0700

    Toying around with giphy's api

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,24 +4,53 @@ import 'preact-material-components/Card/style.css';
 import 'preact-material-components/Button/style.css';
 import style from './style';
 
-export default class Home extends Component {
-	render() {
-		return (
-			<div class={style.home}>
-				<h1>Home route</h1>
-				<Card>
-					<Card.Primary>
-						<Card.Title>Home card</Card.Title>
-						<Card.Subtitle>Welcome to home route</Card.Subtitle>
-					</Card.Primary>
-					<Card.SupportingText>
-						Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-					</Card.SupportingText>
-					<Card.Actions>
-						<Card.Action>OKAY</Card.Action>
-					</Card.Actions>
-				</Card>
-			</div>
-		);
-	}
+class Home extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        };
+    }
+
+    componentWillMount() {
+        const apiUrl = 'https://api.giphy.com';
+        const key = 'xQx0lr9MDJtTm22usyKHwpBHtfwECaNj';
+        const searchUrl = query => `${apiUrl}/v1/gifs/search?api_key=${key}&q=${query}`;
+        fetch(searchUrl('dogs'))
+            .then(s => s.json())
+            .then(j => this.setState({ data: j.data }));
+    }
+
+
+    render() {
+        const { data } = this.state;
+        return(
+            <div class={style.home}>
+                <h1>Homeroute</h1>
+                <Card>
+                    <Card.Primary>
+                        <Card.Title>Homecard</Card.Title>
+                        <Card.Subtitle>Welcometohomeroute</Card.Subtitle>
+                    </Card.Primary>
+                    <Card.SupportingText>
+                        Some text
+                    </Card.SupportingText>
+                    <Card.Actions>
+                        <Card.Action>OKAY</Card.Action>
+                    </Card.Actions>
+                </Card>
+                {
+                    data.map(i => {
+                        const gif = i.images.downsized.url;
+                        return (
+                            <img src={ gif } />
+                        );
+                    })
+                }
+            </div>
+        );
+    }
 }
+
+export default Home;
